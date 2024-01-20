8efe7ac1c6759406baf86f199969a0bf0d4ec50f
SnehaShisNasty <katirina18@gmail.com>
2024-01-20 21:10:36 +0000

    Update Modal.jsx

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -5,7 +5,7 @@ import { Close, ModalCss, Overlay, ImgCss, Button } from './Modal.styled';
 
 const modalRoot = document.getElementById('modal-root');
 
-const Modal = ({ modal, close }) => {
+export const Modal = ({ modal, close }) => {
   const closeModal = ({ target, currentTarget, code }) => {
     if (target === currentTarget || code === 'Escape') {
       close();
@@ -29,5 +29,3 @@ const Modal = ({ modal, close }) => {
     modalRoot
   );
 };
-
-export default Modal;
